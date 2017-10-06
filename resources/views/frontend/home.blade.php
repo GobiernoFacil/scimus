51ea448ab7e3661889a1badccc00e0e14f48eab0
El Santo <hugo.om@gmail.com>
2017-10-06 17:23:51 -0500

    update views

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,358 @@
+<!DOCTYPE html>
+<html lang="es">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>#Sabemos</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{url('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="{{url('vendor/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="{{url('css/freelancer.min.css')}}" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">#Sabemos</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menú
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#help">¿Quieres ayudar?</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">¿Qué es?</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Header -->
+    <header class="masthead">
+      <div class="container">
+        <img class="img-fluid" src="img/sabemos.png" alt="">
+        <div class="intro-text">
+          <span class="name">#sabemos</span>
+          <hr class="star-light">
+          <span class="skills">Sabemos.org, una apuesta de los jóvenes para el largo plazo</span>
+        </div>
+      </div>
+    </header>
+
+    <!-- Portfolio Grid Section -->
+    <section id="help">
+      <div class="container">
+        <h2 class="text-center">¿Quieres ayudar?</h2>
+        <hr class="star-primary">
+        <div class="row">
+          <div class="col-sm-4 portfolio-item">
+	          <h3>Ayuda a comunidad</h3>
+            <a class="portfolio-link" href="#portfolioModal1" data-toggle="modal">
+              <div class="caption">
+                <div class="caption-content">
+                  <i class="fa fa-search-plus fa-3x"></i>
+                </div>
+              </div>
+              <img class="img-fluid" src="{{ url('img/ayudar.jpg')}}" alt="Ayudar en sismo #19s">
+            </a>
+          </div>
+           <div class="col-sm-4 portfolio-item">
+	          <h3>Voluntario</h3>
+            <a class="portfolio-link" href="#portfolioModal3" data-toggle="modal">
+              <div class="caption">
+                <div class="caption-content">
+                  <i class="fa fa-search-plus fa-3x"></i>
+                </div>
+              </div>
+              <img class="img-fluid" src="img/voluntario.jpg"  alt="Ayudar en sismo #19s">
+            </a>
+          </div>
+          <div class="col-sm-4 portfolio-item">
+	          <h3>Directorio</h3>
+            <a class="portfolio-link" href="#portfolioModal2" data-toggle="modal">
+              <div class="caption">
+                <div class="caption-content">
+                  <i class="fa fa-search-plus fa-3x"></i>
+                </div>
+              </div>
+              <img class="img-fluid" src="{{ url('img/contactar.jpg')}}" alt="Ayudar en sismo #19s">
+            </a>
+          </div>
+         
+
+        </div>
+      </div>
+    </section>
+
+    <!-- About Section -->
+    <section class="success" id="about">
+      <div class="container">
+        <h2 class="text-center">¿Qué es?</h2>
+        <hr class="star-light">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <p>sabemos.org es una organización de la sociedad civil y una plataforma de datos abiertos y confiables para que OSC, empresas y administraciones de gobierno puedan tomar mejores decisiones sobre cómo apoyar e incidir en comunidades con distintos proyectos a mediano y/o largo plazo.</p>
+			<p>Estamos trabajando en generar una red de confianza. De este modo, cuidaremos que la información esté corroborada desde la fuente y agilizaremos el proceso para que los facilitadores de recursos orienten estos de manera oportuna mediante alianzas estratégicas para que quienes conforman la red de confianza puedan seguir trabajando en sus proyectos a mediano y largo plazo.</p>
+			<p>Por otro lado, queremos establecer alianzas operativas con universidades, OCS y/o empresas con iniciativas que en el campo del conocimiento y el desarrollo tecnológico que puedan ayudar a fortalecer y alcanzar los objetivos que perseguimos.</p>
+			<p>En sabemos.org creemos firmemente en estas formas de alianza. Estamos apostando por un modelo de trabajo que funja como intermediario entre los que actúan y entre los que tienen la capacidad de proveer los recursos para generar un verdadero impacto en el mediano y largo plazo. Queremos demostrar que los jóvenes organizados y comprometidos podemos seguir participando en la construcción de un país que ponga sus cimientos en la cooperación, solidaridad y compromiso.</p>
+          </div>
+         
+          
+        </div>
+         <hr class="star-light">
+        <h3 class="text-center">¿Cómo lo hacemos?</h3>
+       
+        <div class="row">
+	         <div class="col-lg-8 mx-auto">
+			 	<p>sabemos.org funciona a través de alianzas estratégicas. Somos un puente entre las OCS, empresas y administraciones de gobierno que están incidiendo en las comunidades afectadas (red de confianza) y las OCS, empresas y administraciones gobierno (facilitadores de recursos) que tienen los recursos para ayudar. Asimismo, estamos generando alianzas operativas con universidades, OCS y empresas con iniciativas en el campo del conocimiento y el desarrollo tecnológico para fortalecer y alcanzar los objetivos en común.</p>
+	         </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section id="contact">
+      <div class="container">
+        <h2 class="text-center">Contáctanos</h2>
+        <hr class="star-primary">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
+            <!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
+            <form name="sentMessage" id="contactForm" novalidate>
+              <div class="control-group">
+                <div class="form-group floating-label-form-group controls">
+                  <label>Nombre</label>
+                  <input class="form-control" id="name" type="text" placeholder="Nombre" required data-validation-required-message="Please enter your name.">
+                  <p class="help-block text-danger"></p>
+                </div>
+              </div>
+              <div class="control-group">
+                <div class="form-group floating-label-form-group controls">
+                  <label>Correo</label>
+                  <input class="form-control" id="email" type="email" placeholder="Correo" required data-validation-required-message="Please enter your email address.">
+                  <p class="help-block text-danger"></p>
+                </div>
+              </div>
+              <div class="control-group">
+                <div class="form-group floating-label-form-group controls">
+                  <label>Teléfono</label>
+                  <input class="form-control" id="phone" type="tel" placeholder="Teléfono" required data-validation-required-message="Please enter your phone number.">
+                  <p class="help-block text-danger"></p>
+                </div>
+              </div>
+              <div class="control-group">
+                <div class="form-group floating-label-form-group controls">
+                  <label>Mensaje</label>
+                  <textarea class="form-control" id="message" rows="5" placeholder="Mensaje" required data-validation-required-message="Please enter a message."></textarea>
+                  <p class="help-block text-danger"></p>
+                </div>
+              </div>
+              <br>
+              <div id="success"></div>
+              <div class="form-group">
+                <button type="submit" class="btn btn-success btn-lg" id="sendMessageButton">Enviar</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="text-center">
+      <div class="footer-above">
+        <div class="container">
+          <div class="row">
+            <div class="footer-col col-md-4">
+              <h3>Ubicación</h3>
+              <p>Puebla / Cholula</p>
+            </div>
+            <div class="footer-col col-md-4">
+              <h3>Síguenos</h3>
+              <ul class="list-inline">
+                <li class="list-inline-item">
+                  <a class="btn-social btn-outline" href="https://www.facebook.com/sabemos.org/">
+                    <i class="fa fa-fw fa-facebook"></i>
+                  </a>
+                </li>
+                
+                <li class="list-inline-item">
+                  <a class="btn-social btn-outline" href="https://twitter.com/sabemosorg">
+                    <i class="fa fa-fw fa-twitter"></i>
+                  </a>
+                </li>
+              
+              </ul>
+            </div>
+            <div class="footer-col col-md-4">
+              <h3>#Sabemos</h3>
+              <p>Pronto podrás usar nuestros datos.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer-below">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              Copyright &copy; #Sabemos.org 2017
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-top d-lg-none">
+      <a class="btn btn-primary js-scroll-trigger" href="#page-top">
+        <i class="fa fa-chevron-up"></i>
+      </a>
+    </div>
+
+    <!-- help Modals -->
+    <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="close-modal" data-dismiss="modal">
+            <div class="lr">
+              <div class="rl"></div>
+            </div>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 mx-auto">
+                <div class="modal-body">
+                  <h2>¿Estuviste en una comunidad?</h2>
+                  <hr class="star-primary">
+                  <img class="img-fluid img-centered" src="{{ url('img/ayudar.jpg')}}"  alt="">
+                  <p>Cuéntanos qué necesitan</p>
+                                    <button class="btn btn-success" type="button" data-dismiss="modal">
+                    <i class="fa fa-times"></i>
+                    Cerrar</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="close-modal" data-dismiss="modal">
+            <div class="lr">
+              <div class="rl"></div>
+            </div>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 mx-auto">
+                <div class="modal-body">
+                  <h2>Directorio</h2>
+                  <hr class="star-primary">
+                  <img class="img-fluid img-centered" src="{{ url('img/contactar.jpg')}}" alt="sismo 2017">
+                  <ul class="list-inline item-details">
+				  	<li><strong><a href="https://twitter.com/sosa_ana">Ana Isabel Sosa Machorro</a></strong>
+				  	<span>Directora general							 </span>
+				  	<span>Asistente de alianzas (alianzas operativas)</span></li>
+				  	<li><strong>Lucía Rodríguez Zeledón</strong><br>
+				  	<span>Co-directora							  </span>
+				  	<span>Asistente de alianzas (red de confianza)</span></li>
+				  	<li><strong>Carmen María Cisneros González</strong>
+				  	<span>Coordinadora de alianzas</span></li>
+				  	<li><strong>Ana Dávila de la Macorra</strong>
+				  	<span>Enlace de alianzas (CDMX)</span></li>
+				  	<li><strong>Paola Medina Hernández</strong>
+				  	<span>Coordinadora de investigación</span></li>
+				  	<li><strong>César López</strong>
+				  	<span>Coordinador de archivo fotográfico</span></li>
+				  	<li><strong><a href="https://twitter.com/padaguan">José Flores Sosa</a></strong>
+				  	<span>Coordinador de comunicación</span></li>
+				  	<li><strong>Rodrigo Sosa Machorro</strong>
+				  	<span>Coordinador de diseño corporativo</span></li>
+				  	
+				  	<li><strong><a href="https://github.com/erikriver">Erik Rivera</a></strong>
+				  	<span>Coordinador de levantamiento de datos</span></li>
+				  	<li><strong><a href="http://hugoosorio.com/">Hugo Osorio</a></strong>
+				  	<span><em>Advisor</em></span></li>
+                  </ul>
+                  
+                  <button class="btn btn-success" type="button" data-dismiss="modal">
+                    <i class="fa fa-times"></i>
+                    Cerrar</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="portfolio-modal modal fade" id="portfolioModal3" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="close-modal" data-dismiss="modal">
+            <div class="lr">
+              <div class="rl"></div>
+            </div>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 mx-auto">
+                <div class="modal-body">
+                  <h2>Voluntario</h2>
+                  <hr class="star-primary">
+                  <img class="img-fluid img-centered" src="img/voluntario.jpg" alt="">
+                  <p>¿Quieres ser voluntario?</p>
+                  <button class="btn btn-success" type="button" data-dismiss="modal">
+                    <i class="fa fa-times"></i>
+                    Cerrar</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="{{url('vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{url('vendor/popper/popper.min.js')}}"></script>
+    <script src="{{url('vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="{{url('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="{{url('js/jqBootstrapValidation.js')}}"></script>
+    <script src="{{url('js/contact_me.js')}}"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="{{url('js/freelancer.min.js')}}"></script>
+
+  </body>
+
+</html>
